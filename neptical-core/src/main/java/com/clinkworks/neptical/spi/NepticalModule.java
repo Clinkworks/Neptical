27f28c2c89b80c60205b1f36700f9021ed3b7582
Anthony Clink <Anthony Clink>
2016-06-17 19:57:14 -0700

    removed previous implementation from repo

--- neptical-core/src/main/java/com/clinkworks/neptical/spi/NepticalModule.java
+++
@@ -1,7 +0,0 @@
-package com.clinkworks.neptical.spi;
-
-import com.google.inject.Module;
-
-public interface NepticalModule extends Module{
-
-}
--- neptical-core/src/main/java/com/clinkworks/neptical/util/SpringInjectionUtil.java
+++
@@ -1,5 +0,0 @@
-package com.clinkworks.neptical.util;
-
-public class SpringInjectionUtil {
-
-}
